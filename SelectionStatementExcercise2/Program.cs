837cc014ed63dbb06767a4db4c60ed547ad67b8d
Sean Morgan <scm0323@att.net>
2021-08-20 15:46:20 -0500

    selection excercise 2 switch/case

---
+++ SelectionStatementExcercise2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SelectionStatementExcercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject in school?");
+            string userSub = Console.ReadLine();
+
+            switch(userSub)
+            {
+                case "programming":
+                    Console.WriteLine("Very nice subject.");
+                    break;
+
+                case "math":
+                    Console.WriteLine("Sounds it could be interesting.");
+                    break;
+
+                case "english":
+                    Console.WriteLine("Sounds mainstream.");
+                    break;
+
+                case "physics":
+                    Console.WriteLine("Sounds difficult.");
+                    break;
+
+                case "economics":
+                    Console.WriteLine("Man that is a boring subject.");
+                    break;
+
+                default:
+                    Console.WriteLine("Pick a different subject.");
+                    break;
+            }
+        }
+    }
+}
